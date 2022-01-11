231dc69aac2631796a1b114fccafe875e115c639
Vinit Kothari <45110458+Vkothari28@users.noreply.github.com>
2022-01-11 15:43:26 +0400

    Delete codemr/Chess directory

--- codemr/Chess/html/main_report/htmlx/modeldata/overview_metric_data.js
+++
@@ -1,1 +0,0 @@
-var CodeMR_ProjectMetrics = {"Project Name":"Chess", "Number of Highly Problematic Classes":0, "Number of Entities":12, "Number of Problematic Classes":1, "Class Lines of Code":698, "Number of External Packages":25, "Number of Packages":4, "Number of External Entities":171}
